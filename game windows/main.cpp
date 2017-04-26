5605f5624b444d64820a5a82fc61f8a3968f77aa
freelancerhu <freelancerhu01@gmail.com>
2017-04-26 13:57:33 -0400

    update class

--- game windows/main.cpp
+++ game windows/main.cpp
@@ -29,152 +29,229 @@ struct coord {
   coord(int x1, int y1): x(x1), y(y1){}
 };
 
-void printMap(std::vector<coord> &A, std::vector<coord> &eggVec, int sMap[][20], int len) {
-  for (auto z : A) {
-    sMap[z.x][z.y] = 1;
+
+class Map {
+public:
+  Map() {}
+
+  ~Map() {
+    gamemap_.clear();
   }
-  sMap[eggVec[0].x][eggVec[0].y] = 7;
-  for (int i = 0; i < len; ++i) {
-    for (int j = 0; j < len; ++j) {
-      std::cout << sMap[i][j];
+
+  void Reset_Map() {
+    for (size_t  i = 0; i < gamemap_.size(); ++i) {
+        for (size_t j = 0; j < gamemap_[0].size(); ++j) {
+            gamemap_[i][j] = 0;
+          }
+      }
+  }
+
+  void Snk_On_Map(const std::vector<coord> &snavec_) {
+    for (auto z : snavec_) {
+      gamemap_[z.x][z.y] = 1;
     }
-    std::cout << std::endl;
-  }
-  std::cout << "Length of your Snake : " << A.size() << std::endl;
-}
-
-bool controlSnake(std::vector<coord> &A, std::vector<coord> &eggVec, char index) {
-  int dx = 0, dy = 0;
-
-  static int delta[][2] = {
-    {-1, 0},
-    {1, 0},
-    {0, -1},
-    {0, 1}};
-
-  int direction = 0;
-  switch (index) {
-    case 'w':
-      direction = 0;
-      break;
-    case 's':
-      direction = 1;
-      break;
-    case 'a':
-      direction = 2;
-      break;
-    case 'd':
-      direction = 3;
-      break;
-    default:
-      ;
   }
 
-  dx = delta[direction][0];
-  dy = delta[direction][1];
-
-  if (A[0].x+dx<0 || A[0].y+dy<0 ||
-      A[0].x+dx>=20 || A[0].y+dy>=20)
-    return false;
-
-  for (int i = A.size() - 1; i > 0; --i) {
-    A[i].x = A[i - 1].x;
-    A[i].y = A[i - 1].y;
+  void Egg_On_Map(const std::vector<coord> &eggvec_) {
+    for (auto z : eggvec_) {
+      gamemap_[z.x][z.y] = 2;
+    }
   }
 
-  A[0].x = A[0].x + dx;
-  A[0].y = A[0].y + dy;
-  for (int i = 1; i < A.size(); ++i) {
-      if (A[0].x == A[i].x && A[0].y == A[i].y)
-          return false;
+  void Init_Map(int x, int y) {
+    for (int i = 0; i < y; ++i) {
+        std::vector<int> temp;
+      for (int j = 0; j < x; ++j) {
+        temp.push_back(0);
+      }
+      gamemap_.push_back(temp);
+    }
   }
 
-  if (A[0].x == eggVec[0].x && A[0].y == eggVec[0].y) {
-    eggVec.clear();
-    int tempX = A.back().x, tempY = A.back().y;
-    A.push_back(coord(tempX, tempY));
+  int Map_Row() const {
+    return gamemap_.size();
   }
 
-  return true;
-}
+  int Map_column() const {
+    return gamemap_[0].size();
+  }
 
-void layEgg(std::vector<coord> &snaVec, std::vector<coord> &eggVec) {
-  if (eggVec.empty()){
-    for (;;) {
-      int vx = rand() % 19 + 0;
-      int vy = rand() % 19 + 0;
-      int i = 0;
-      for (auto z : snaVec) {
-        if (z.x == vx && z.y == vy) {
-          ++i;
+  int Map_Val(int &x, int &y) const {
+    return gamemap_[x][y];
+  }
+
+private:
+  std::vector<std::vector<int>> gamemap_;
+};
+
+class printMap {
+public:
+  void Print_Map(const Map &map) {
+    for (int i = 0; i < map.Map_Row(); ++i) {
+      for (int j = 0; j < map.Map_column(); ++j) {
+        std::cout << map.Map_Val(i, j);
+      }
+      std::cout << std::endl;
+    }
+  }
+};
+
+class controlSnake {
+public:
+  bool Control_Snake(std::vector<coord> &A, std::vector<coord> &eggVec, char index) {
+    int dx = 0, dy = 0;
+    static int delta[][2] = {
+      {-1, 0},
+      {1, 0},
+      {0, -1},
+      {0, 1}};
+    int direction = 0;
+    switch (index) {
+      case 'w':
+        direction = 0;
+        break;
+      case 's':
+        direction = 1;
+        break;
+      case 'a':
+        direction = 2;
+        break;
+      case 'd':
+        direction = 3;
+        break;
+      default:
+        ;
+    }
+    dx = delta[direction][0];
+    dy = delta[direction][1];
+    if (A[0].x+dx<0 || A[0].y+dy<0 ||
+        A[0].x+dx>=20 || A[0].y+dy>=20)
+      return false;
+
+    for (int i = A.size() - 1; i > 0; --i) {
+      A[i].x = A[i - 1].x;
+      A[i].y = A[i - 1].y;
+    }
+    A[0].x = A[0].x + dx;
+    A[0].y = A[0].y + dy;
+    for (int i = 1; i < A.size(); ++i) {
+        if (A[0].x == A[i].x && A[0].y == A[i].y)
+            return false;
+    }
+
+    if (A[0].x == eggVec[0].x && A[0].y == eggVec[0].y) {
+      eggVec.clear();
+      int tempX = A.back().x, tempY = A.back().y;
+      A.push_back(coord(tempX, tempY));
+    }
+    return true;
+  }
+};
+
+class Model {
+public:
+  Model() {}
+  ~Model() {
+    snavec_.clear();
+    eggvec_.clear();
+  }
+
+  void Gene_Snake() {
+    snavec_.push_back(coord(5, 7));
+    snavec_.push_back(coord(5, 6));
+    snavec_.push_back(coord(5, 5));
+    snavec_.push_back(coord(5, 4));
+    snavec_.push_back(coord(5, 3));
+    map_.Snk_On_Map(snavec_);
+  }
+
+  void Gene_Egg() {
+    if (eggvec_.empty()){
+      for (;;) {
+        int vx = rand() % 19 + 0;
+        int vy = rand() % 19 + 0;
+        int i = 0;
+        for (auto z : snavec_) {
+          if (z.x == vx && z.y == vy) {
+            ++i;
+          }
+        }
+        if (i == 0) {
+          eggvec_.push_back(coord(vx, vy));
+          map_.Egg_On_Map(eggvec_);
+          return;
         }
       }
-      if (i == 0) {
-        eggVec.push_back(coord(vx, vy));
-        return;
-      }
     }
   }
-}
 
+  void Set_Map(int x, int y) {
+    map_.Init_Map(x, y);
+  }
 
-
-int main() {
-  srand(time(NULL));
-
-  std::vector<coord> snaVec;
-  snaVec.push_back(coord(5, 7)); // row  column
-  snaVec.push_back(coord(5, 6));
-  snaVec.push_back(coord(5, 5));
-  snaVec.push_back(coord(5, 4));
-  snaVec.push_back(coord(5, 3));
-
-  std::vector<coord> eggVec;
-  char index = 'd';
-  for (;;) {
+  bool Run(char &index) {
     system("cls");
-    int sMap[20][20]{0};
-    layEgg(snaVec, eggVec);
-    printMap(snaVec, eggVec, sMap, 20);
+    Gene_Egg();
+    map_.Reset_Map();
+    map_.Snk_On_Map(snavec_);
+    map_.Egg_On_Map(eggvec_);
+    printmap_.Print_Map(map_);
+    std::cout << "Length of your Snake : " << snavec_.size() << std::endl;
     std::chrono::duration<double, std::milli> ms(100);
     std::this_thread::sleep_for(ms);
     if (_kbhit()) {
       char tempIndex = _getch();
       switch (tempIndex) {
         case 'w':
-          if (snaVec[1].x - snaVec[0].x != -1)
+          if (snavec_[1].x - snavec_[0].x != -1)
             index = tempIndex;
           break;
         case 's':
-          if (snaVec[1].x - snaVec[0].x != 1)
+          if (snavec_[1].x - snavec_[0].x != 1)
             index = tempIndex;
           break;
         case 'a':
-          if (snaVec[1].y - snaVec[0].y != -1)
+          if (snavec_[1].y - snavec_[0].y != -1)
             index = tempIndex;
           break;
         case 'd':
-          if (snaVec[1].y - snaVec[0].y != 1)
+          if (snavec_[1].y - snavec_[0].y != 1)
             index = tempIndex;
           break;
         default:
           ;
       }
-
-      if (controlSnake(snaVec, eggVec, index)) {
-          continue;
+      if (controlsnake_.Control_Snake(snavec_, eggvec_, index)) {
+          return true;
       } else {
-          break;
+          return false;
       }
     } else {
-      if (controlSnake(snaVec, eggVec,index)) {
-          continue;
+      if (controlsnake_.Control_Snake(snavec_, eggvec_, index)) {
+          return true;
       } else {
-          break;
+          return false;
       }
     }
   }
-  std::cout << "GG" << std::endl;
 
+private:
+  Map map_;
+  printMap printmap_;
+  controlSnake controlsnake_;
+  std::vector<coord> snavec_;
+  std::vector<coord> eggvec_;
+};
+
+
+
+int main() {
+  srand(time(NULL));
+  Model model;
+  model.Set_Map(20, 20);
+  model.Gene_Snake();
+  char index = 'd';
+  while (model.Run(index));
+  std::cout << "GG" << std::endl;
   return 0;
 }
